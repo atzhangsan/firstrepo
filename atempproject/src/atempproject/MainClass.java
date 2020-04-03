e33c207d3ce3eac42a254d2183392236cee29a36
atzhangsan <675742869@qq.com>
2020-04-03 17:10:55 +0800

    Update MainClass.java

--- atempproject/src/atempproject/MainClass.java
+++ atempproject/src/atempproject/MainClass.java
@@ -3,6 +3,8 @@ package atempproject;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("print something");
+		System.out.println("print 222something");
+		
 	}
 
 }
